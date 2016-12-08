427f442e810e7b1f0d6a172954abd0a65d403663
lunaan18-meet <luna.an136@gmail.com>
2016-12-08 19:55:42 +0200

    stopping hear

---
+++ student.py
@@ -0,0 +1,17 @@
+class students:
+    def __init__(self,name,hometown,age,height,favorite_ice_cream_flavor):
+        self.name=name
+        self.hometown=hometown
+        self.age=age
+        self.height=height
+        self.favorite_ice_cream_flavor=favorite_ice_cream_flavor
+    def print_summary :
+        print(self.name)
+        print(self.hometown)
+        print(age)
+        print(height)
+        print(favorite_ice_cream_flavor)
+    def get_giraffe_gap (self):
+        avg_giraffe=500
+        return avg_giraffe-self.height
+
